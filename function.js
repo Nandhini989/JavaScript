bb14d21ce8eda5f5b363af2bdb72fa6743a90202
Nandhini989 <nandinikondu9@gmail.com>
2025-09-22 14:18:18 +0530

    function

---
+++ function.js
@@ -0,0 +1,33 @@
+// function add(){
+//     var a=17;
+//     var b=67;
+//     document.write(a+b);
+//     console.log(a+b);
+// };
+// add();
+
+//es6
+
+// const add=()=>{
+//     var a=17;
+//     var b=77;
+//     document.write(a+b);
+//     console.log(a+b);
+// };
+// add();
+ 
+//ES7
+
+// const add=(a,b)=> console.log(a+b);
+// add(28,10);
+
+// console.log(b);
+// b=45;
+
+let name="nandhu";
+function demo(){
+    let name="nani";
+    document.write('hello, my name is ${name}');
+}
+    document.write('hello, my name is ${name}');
+demo();
