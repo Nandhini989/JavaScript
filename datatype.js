c927f710150dab1c31fd1b4756082ff6b9f4bc79
Nandhini989 <nandinikondu9@gmail.com>
2025-09-22 11:02:03 +0530

    datatype

---
+++ datatype.js
@@ -0,0 +1,18 @@
+a=7894;
+console.log(typeof a);
+name="Nandhu";
+console.log(typeof name);
+c=true;
+console.log(typeof c);
+data=null;
+console.log(typeof data);
+d=undefined;
+console.log(typeof d);
+array=[56, "nani",true,null];
+console.log(typeof array);
+obj={
+    name:"nandhu",
+    age:19,
+    dept:"CSE",
+};
+console.log(typeof obj);
