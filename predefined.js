edec7ec838cfdc48b38b4f631428e0df63004df9
Nandhini989 <nandinikondu9@gmail.com>
2025-09-22 16:11:45 +0530

    predefined

---
+++ predefined.js
@@ -0,0 +1,22 @@
+var a=Number(prompt("enter value a "));
+var b=Number(prompt("enter value b "));
+document.write(`entered value of a and b is ${a},${b}`);
+function add(){
+    alert(a+b);
+}
+function sub(){
+    alert(a-b);
+}
+function mul(){
+    alert(a*b);
+}
+function div(){
+    alert(a/b);
+}
+
+
+
+// document.write(alert(a+b));
+// document.write(alert(a-b));
+// document.write(alert(a*b));
+// document.write(alert(a%b));
