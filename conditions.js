d89fcca1e293e33e4ac7b7ecc1b087b4db485229
Nandhini989 <nandinikondu9@gmail.com>
2025-09-23 11:30:02 +0530

    indexx

---
+++ conditions.js
@@ -0,0 +1,13 @@
+var a=prompt("enter the age ");
+// if(a<18){
+//     document.write("you are not eligible for the browsing");
+// }
+// else{
+//     document.write("you are eligible");
+// }
+if(age<=45 & age>65){
+    document.write("you are senior citizen");
+}
+else{
+    document.write("you are youth");
+}
